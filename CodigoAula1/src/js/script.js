d2d8f1764d34aec529c78b7df68174858b357624
Júlio Cézar <juliocezarpvh@hotmail.com>
2024-09-26 23:28:45 -0400

    Commit do projeto em HTML, CSS e JavaScript feito em aula no dia 23/09

--- CodigoAula1/src/js/script.js
+++ CodigoAula1/src/js/script.js
@@ -1,25 +1,3 @@
-window.addEventListener('scroll', function() {
-    const parallaxContainer = document.querySelector('.parallax-container');
-    let scrollPosition = window.scrollY; 
-    parallaxContainer.style.backgroundPositionY = scrollPosition * 0.5 + 'px'; 
-});
-
-const today = new Date(); // <-- Data atual
-const birthday = new Date(today.getFullYear(), 8, 17); // <-- Data do meu aniversário, mês de setembro (8) e dia 17
-
-// Verificando se o aniversário já aconteceu neste ano
-if (
-    today.getMonth() < 8 || // <-- Mês atual antes de setembro
-    (today.getMonth() === 8 && today.getDate() < 17) // <-- Mês atual é setembro e o dia atual é antes do dia 17
-) {
-    birthday.setFullYear(today.getFullYear() - 1); // <-- Se o aniversário ainda não ocorreu, é ajustado para o ano passado
-}
-// Calcula a idade
-let age = today.getFullYear() - birthday.getFullYear();
-
-// Span com atualização da minha idade
-document.getElementById('age').textContent = age;
-
 // Span com atualização automática do ano para o rodapé
 document.getElementById('current-year').textContent = new Date().getFullYear();
 
